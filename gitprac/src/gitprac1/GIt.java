a3e2a17fc12219543b0135e78fbb79da24cb8e45
rammadhav9 <139705582+rammadhav9@users.noreply.github.com>
2023-07-16 23:24:42 +0530

    commit1

--- gitprac/src/gitprac1/GIt.java
+++ gitprac/src/gitprac1/GIt.java
@@ -5,7 +5,7 @@ public class GIt {
 	public static void main(String[] args) {
 		
 		System.out.println("hiii");
-		System.out.println("hello worldddd");
+		System.out.println("hello worldddd iugliggj");
 
 	}
 
